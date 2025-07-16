cdc09b78e38f560a763d87845f1c2d2f04e2b992
mahin <build_03@akijmatch.com>
2025-07-16 22:58:04 +0600

    paw up

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import React, { useEffect } from "react";
-
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   useEffect(() => {
@@ -17,6 +17,12 @@ export default function RootLayout({ children }) {
   }, []);
   return (
     <html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#317EFB" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="mobile-web-app-capable" content="yes" />
+      </Head>
       <body>
         {children}
         <Toaster position="top-right" />
